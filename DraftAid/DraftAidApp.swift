cb5a909569239b54c1dcb6e4505d211be19be494
boomNDS <boomsixboom@gmail.com>
2026-09-02 00:11:06 +0700

    Initial Commit

---
+++ DraftAid/DraftAidApp.swift
@@ -0,0 +1,32 @@
+//
+//  DraftAidApp.swift
+//  DraftAid
+//
+//  Created by Pa Sri on 2/9/2569 BE.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DraftAidApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
